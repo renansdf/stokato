b7c7cee61c750da526a27bcf0dd240e2f5643189
Renan de Freitas <rnn.frts@gmail.com>
2021-07-22 15:48:54 -0300

    created class to manipulate mongo atlas

---
+++ src/services/MongoDatabaseService.ts
@@ -0,0 +1,59 @@
+import { MongoClient } from 'mongodb';
+import connection from '../helpers/connection';
+
+class MongoDatabaseService {
+  private client: MongoClient;
+  private database: string;
+  private collection: string;
+
+  constructor (dbName: string, collectionName: string){
+    this.client = connection;
+    this.database = dbName;
+    this.collection = collectionName;
+  }
+
+  public async create (object: Object) {
+    let response;
+
+    try{
+      await this.client.connect();
+  
+      const db = this.client.db(this.database);
+      const collection = db.collection(this.collection);
+  
+      await collection.insertOne(object);
+      response = {success: 'object inserted into database'}
+    } catch(e) {
+      response = {error: e};
+    } finally {
+      await this.client.close();
+    }
+
+    return response;
+  }
+
+  public async getAll (){
+    let data: any = [];
+
+    try{
+      await this.client.connect();
+  
+      const db = this.client.db(this.database);
+      const collection = db.collection(this.collection);
+  
+      const cursor = collection.find();
+      
+      await cursor.forEach((element) => {
+        data.push(element);
+      });
+    } catch(e) {
+      data.push({error: e});
+    } finally {
+      await this.client.close();
+    }
+
+    return data;
+  }
+}
+
+export default MongoDatabaseService;
