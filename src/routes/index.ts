7d3f8943e0a97e87f90d1fe344b71280a3bde336
Renan de Freitas <rnn.frts@gmail.com>
2021-08-12 18:43:32 -0300

    added bugeto router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+
+import baseRouter from './baseRouter';
 import quoteRouter from './quoteRouter';
 import contactRouter from './contactRouter';
-import baseRouter from './baseRouter';
+import signoRouter from './signoRouter';
 
 const routes = Router();
 
+routes.use('/', baseRouter);
 routes.use('/quote', quoteRouter);
 routes.use('/contact', contactRouter);
-routes.use('/', baseRouter);
+routes.use('/signo', signoRouter);
 
 export default routes;
